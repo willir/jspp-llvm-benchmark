f646f64d79c0ac05686a55f7c11c5a0594fd9c8d
Anton Rapetov <willir29@yandex.ru>
2019-08-25 23:21:11 +0300

    Refactor: Use wrapping_OP instead of overflowing_OP

--- murmur.rs
+++ murmur.rs
@@ -28,12 +28,12 @@ pub extern "C" fn murmur_rust(key: *const Arr, seed: u32) -> u32 {
             k <<= 8;
             k |= key[i] as u32;
 
-            k = k.overflowing_mul(0xcc9e2d51).0;
+            k = k.wrapping_mul(0xcc9e2d51);
             k = k.rotate_left(15);
-            k = k.overflowing_mul(0x1b873593).0;
+            k = k.wrapping_mul(0x1b873593);
             h ^= k;
             h = h.rotate_left(13);
-            h = h.overflowing_mul(5).0.overflowing_add(0xe6546b64).0;
+            h = h.wrapping_mul(5).wrapping_add(0xe6546b64);
         }
     }
     if (key.len() & 3) != 0 {
@@ -49,16 +49,16 @@ pub extern "C" fn murmur_rust(key: *const Arr, seed: u32) -> u32 {
             }
         }
 
-        k = k.overflowing_mul(0xcc9e2d51).0;
+        k = k.wrapping_mul(0xcc9e2d51);
         k = k.rotate_left(15);
-        k = k.overflowing_mul(0x1b873593).0;
+        k = k.wrapping_mul(0x1b873593);
         h ^= k;
     }
     h ^= key.len() as u32;
     h ^= h >> 16;
-    h = h.overflowing_mul(0x85ebca6b).0;
+    h = h.wrapping_mul(0x85ebca6b);
     h ^= h >> 13;
-    h = h.overflowing_mul(0xc2b2ae35).0;
+    h = h.wrapping_mul(0xc2b2ae35);
     h ^= h >> 16;
 
     h
@@ -78,9 +78,9 @@ pub extern "C" fn murmur_rust2(key: *const Arr, seed: u32) -> u32 {
 
     h ^= key.len() as u32;
     h ^= h >> 16;
-    h = h.overflowing_mul(0x85ebca6b).0;
+    h = h.wrapping_mul(0x85ebca6b);
     h ^= h >> 13;
-    h = h.overflowing_mul(0xc2b2ae35).0;
+    h = h.wrapping_mul(0xc2b2ae35);
     h ^= h >> 16;
 
     h
@@ -90,7 +90,7 @@ fn update_from_mid_chunk(chunk: &[u8], mut h: u32) -> u32 {
     h ^= buf_to_k(chunk.try_into().expect("4-element slice expected"));
 
     h = h.rotate_left(13);
-    h = h.overflowing_mul(5).0.overflowing_add(0xe6546b64).0;
+    h = h.wrapping_mul(5).wrapping_add(0xe6546b64);
 
     h
 }
@@ -104,9 +104,7 @@ fn update_from_last_chunk(chunk: &[u8], h: u32) -> u32 {
 
 fn buf_to_k(chunk: [u8; 4]) -> u32 {
     u32::from_le_bytes(chunk)
-        .overflowing_mul(0xcc9e2d51)
-        .0
+        .wrapping_mul(0xcc9e2d51)
         .rotate_left(15)
-        .overflowing_mul(0x1b873593)
-        .0
+        .wrapping_mul(0x1b873593)
 }
